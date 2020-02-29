ddd7524c2f8f70052d534f2fccd126cf76f9b5e8
Alexis Lyndon Galaura <alexislyndon@gmail.com>
2020-02-29 13:09:30 +0800

    Dashboard transitions looking sweet

--- ADMIN.Designer.vb
+++ ADMIN.Designer.vb
@@ -22,15 +22,181 @@ Partial Class ADMIN
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.sidepanel = New System.Windows.Forms.Panel()
+        Me.Panel4 = New System.Windows.Forms.Panel()
+        Me.Panel3 = New System.Windows.Forms.Panel()
+        Me.Panel2 = New System.Windows.Forms.Panel()
         Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.dashbtn = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.Timer2 = New System.Windows.Forms.Timer(Me.components)
+        Me.Timer3 = New System.Windows.Forms.Timer(Me.components)
+        Me.Timer4 = New System.Windows.Forms.Timer(Me.components)
+        Me.sidepanel.SuspendLayout()
         Me.SuspendLayout()
+        '
+        'sidepanel
+        '
+        Me.sidepanel.BackColor = System.Drawing.Color.FromArgb(CType(CType(35, Byte), Integer), CType(CType(35, Byte), Integer), CType(CType(35, Byte), Integer))
+        Me.sidepanel.Controls.Add(Me.Panel4)
+        Me.sidepanel.Controls.Add(Me.Panel3)
+        Me.sidepanel.Controls.Add(Me.Panel2)
+        Me.sidepanel.Controls.Add(Me.Panel1)
+        Me.sidepanel.Controls.Add(Me.Label2)
+        Me.sidepanel.Controls.Add(Me.Button3)
+        Me.sidepanel.Controls.Add(Me.Button2)
+        Me.sidepanel.Controls.Add(Me.Button1)
+        Me.sidepanel.Controls.Add(Me.dashbtn)
+        Me.sidepanel.Controls.Add(Me.Label1)
+        Me.sidepanel.Location = New System.Drawing.Point(0, 0)
+        Me.sidepanel.Name = "sidepanel"
+        Me.sidepanel.Size = New System.Drawing.Size(200, 500)
+        Me.sidepanel.TabIndex = 0
+        '
+        'Panel4
+        '
+        Me.Panel4.BackColor = System.Drawing.Color.FromArgb(CType(CType(80, Byte), Integer), CType(CType(180, Byte), Integer), CType(CType(200, Byte), Integer))
+        Me.Panel4.ForeColor = System.Drawing.Color.Coral
+        Me.Panel4.Location = New System.Drawing.Point(10, 260)
+        Me.Panel4.Name = "Panel4"
+        Me.Panel4.Size = New System.Drawing.Size(5, 35)
+        Me.Panel4.TabIndex = 3
+        Me.Panel4.Visible = False
+        '
+        'Panel3
+        '
+        Me.Panel3.BackColor = System.Drawing.Color.FromArgb(CType(CType(80, Byte), Integer), CType(CType(180, Byte), Integer), CType(CType(200, Byte), Integer))
+        Me.Panel3.ForeColor = System.Drawing.Color.Coral
+        Me.Panel3.Location = New System.Drawing.Point(10, 220)
+        Me.Panel3.Name = "Panel3"
+        Me.Panel3.Size = New System.Drawing.Size(5, 35)
+        Me.Panel3.TabIndex = 3
+        Me.Panel3.Visible = False
+        '
+        'Panel2
+        '
+        Me.Panel2.BackColor = System.Drawing.Color.FromArgb(CType(CType(80, Byte), Integer), CType(CType(180, Byte), Integer), CType(CType(200, Byte), Integer))
+        Me.Panel2.ForeColor = System.Drawing.Color.Coral
+        Me.Panel2.Location = New System.Drawing.Point(10, 180)
+        Me.Panel2.Name = "Panel2"
+        Me.Panel2.Size = New System.Drawing.Size(5, 35)
+        Me.Panel2.TabIndex = 3
+        Me.Panel2.Visible = False
         '
         'Panel1
         '
-        Me.Panel1.Location = New System.Drawing.Point(0, 0)
+        Me.Panel1.Anchor = System.Windows.Forms.AnchorStyles.None
+        Me.Panel1.BackColor = System.Drawing.Color.FromArgb(CType(CType(80, Byte), Integer), CType(CType(180, Byte), Integer), CType(CType(200, Byte), Integer))
+        Me.Panel1.ForeColor = System.Drawing.Color.Coral
+        Me.Panel1.Location = New System.Drawing.Point(10, 140)
         Me.Panel1.Name = "Panel1"
-        Me.Panel1.Size = New System.Drawing.Size(200, 500)
-        Me.Panel1.TabIndex = 0
+        Me.Panel1.Size = New System.Drawing.Size(5, 35)
+        Me.Panel1.TabIndex = 3
+        Me.Panel1.Visible = False
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.ForeColor = System.Drawing.Color.White
+        Me.Label2.Location = New System.Drawing.Point(13, 54)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(68, 13)
+        Me.Label2.TabIndex = 2
+        Me.Label2.Text = "Logged in as"
+        '
+        'Button3
+        '
+        Me.Button3.FlatAppearance.BorderSize = 0
+        Me.Button3.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(CType(CType(20, Byte), Integer), CType(CType(20, Byte), Integer), CType(CType(20, Byte), Integer))
+        Me.Button3.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(CType(CType(10, Byte), Integer), CType(CType(10, Byte), Integer), CType(CType(10, Byte), Integer))
+        Me.Button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button3.Font = New System.Drawing.Font("Microsoft Tai Le", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button3.ForeColor = System.Drawing.Color.Silver
+        Me.Button3.Location = New System.Drawing.Point(16, 260)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(184, 35)
+        Me.Button3.TabIndex = 1
+        Me.Button3.Text = "Users"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.FlatAppearance.BorderSize = 0
+        Me.Button2.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(CType(CType(20, Byte), Integer), CType(CType(20, Byte), Integer), CType(CType(20, Byte), Integer))
+        Me.Button2.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(CType(CType(10, Byte), Integer), CType(CType(10, Byte), Integer), CType(CType(10, Byte), Integer))
+        Me.Button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button2.Font = New System.Drawing.Font("Microsoft Tai Le", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button2.ForeColor = System.Drawing.Color.Silver
+        Me.Button2.Location = New System.Drawing.Point(16, 220)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(184, 35)
+        Me.Button2.TabIndex = 1
+        Me.Button2.Text = "Vehicles"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button1
+        '
+        Me.Button1.FlatAppearance.BorderSize = 0
+        Me.Button1.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(CType(CType(20, Byte), Integer), CType(CType(20, Byte), Integer), CType(CType(20, Byte), Integer))
+        Me.Button1.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(CType(CType(10, Byte), Integer), CType(CType(10, Byte), Integer), CType(CType(10, Byte), Integer))
+        Me.Button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button1.Font = New System.Drawing.Font("Microsoft Tai Le", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button1.ForeColor = System.Drawing.Color.Silver
+        Me.Button1.Location = New System.Drawing.Point(16, 180)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(184, 35)
+        Me.Button1.TabIndex = 1
+        Me.Button1.Text = "Visitors"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'dashbtn
+        '
+        Me.dashbtn.FlatAppearance.BorderSize = 0
+        Me.dashbtn.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(CType(CType(20, Byte), Integer), CType(CType(20, Byte), Integer), CType(CType(20, Byte), Integer))
+        Me.dashbtn.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(CType(CType(10, Byte), Integer), CType(CType(10, Byte), Integer), CType(CType(10, Byte), Integer))
+        Me.dashbtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.dashbtn.Font = New System.Drawing.Font("Microsoft Tai Le", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.dashbtn.ForeColor = System.Drawing.Color.Silver
+        Me.dashbtn.Image = Global.SecurityBook.My.Resources.Resources._2618259
+        Me.dashbtn.Location = New System.Drawing.Point(16, 140)
+        Me.dashbtn.Name = "dashbtn"
+        Me.dashbtn.Size = New System.Drawing.Size(184, 35)
+        Me.dashbtn.TabIndex = 1
+        Me.dashbtn.Text = "Dashboard"
+        Me.dashbtn.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Lucida Sans Unicode", 18.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.ForeColor = System.Drawing.Color.White
+        Me.Label1.Location = New System.Drawing.Point(8, 10)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(186, 28)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "ADMIN PANEL"
+        '
+        'Timer1
+        '
+        Me.Timer1.Interval = 10
+        '
+        'Timer2
+        '
+        Me.Timer2.Interval = 10
+        '
+        'Timer3
+        '
+        Me.Timer3.Interval = 10
+        '
+        'Timer4
+        '
+        Me.Timer4.Interval = 10
         '
         'ADMIN
         '
@@ -38,13 +204,30 @@ Partial Class ADMIN
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(1000, 500)
         Me.ControlBox = False
-        Me.Controls.Add(Me.Panel1)
+        Me.Controls.Add(Me.sidepanel)
         Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
         Me.Name = "ADMIN"
         Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "w"
+        Me.sidepanel.ResumeLayout(False)
+        Me.sidepanel.PerformLayout()
         Me.ResumeLayout(False)
 
     End Sub
 
+    Friend WithEvents sidepanel As Panel
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents dashbtn As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Panel4 As Panel
+    Friend WithEvents Panel3 As Panel
+    Friend WithEvents Panel2 As Panel
     Friend WithEvents Panel1 As Panel
+    Friend WithEvents Timer1 As Timer
+    Friend WithEvents Timer2 As Timer
+    Friend WithEvents Timer3 As Timer
+    Friend WithEvents Timer4 As Timer
 End Class
--- ADMIN.vb
+++ ADMIN.vb
@@ -2,6 +2,14 @@
     Private IsFormBeingDragged As Boolean = False
     Private MouseDownX As Integer
     Private MouseDownY As Integer
+    Dim tem As Point = New Point()
+
+    Dim y1 As Integer = 140
+    Dim y2 As Integer = 180
+    Dim y3 As Integer = 220
+    Dim y4 As Integer = 260
+
+
 
     Private Sub Form1_MouseDown(ByVal sender As Object, ByVal e As MouseEventArgs) Handles MyBase.MouseDown
 
@@ -29,6 +37,93 @@
             Me.Location = temp
             temp = Nothing
         End If
+    End Sub
+
+    Private Sub ADMIN_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Panel1.Visible = True
+        tem.X = 10
+
+    End Sub
+
+    Private Sub dashbtn_Click(sender As Object, e As EventArgs) Handles dashbtn.Click
+        falsifier()
+        tem.Y = Panel1.Location.Y
+        Timer1.Start()
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        falsifier()
+        tem.Y = Panel1.Location.Y
+        Timer2.Start()
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        falsifier()
+        tem.Y = Panel1.Location.Y
+        Timer3.Start()
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        falsifier()
+        tem.Y = Panel1.Location.Y
+        Timer4.Start()
+    End Sub
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        If tem.Y > y1 Then
+            tem.Y -= 5
+        End If
+        If tem.Y < y1 Then
+            tem.Y += 5
+        End If
+        If tem.Y = y1 Then
+            Timer1.Stop()
+        End If
+
+        Panel1.Location = tem
+    End Sub
+
+    Private Sub Timer2_Tick(sender As Object, e As EventArgs) Handles Timer2.Tick
+        If tem.Y > y2 Then
+            tem.Y -= 5
+        End If
+        If tem.Y < y2 Then
+            tem.Y += 5
+        End If
+        If tem.Y = y2 Then
+            Timer2.Stop()
+        End If
+        Panel1.Location = tem
+    End Sub
+
+    Private Sub Timer3_Tick(sender As Object, e As EventArgs) Handles Timer3.Tick
+        If tem.Y > y3 Then
+            tem.Y -= 5
+        End If
+        If tem.Y < y3 Then
+            tem.Y += 5
+        End If
+        If tem.Y = y3 Then
+            Timer3.Stop()
+        End If
+        Panel1.Location = tem
+    End Sub
+
+    Private Sub Timer4_Tick(sender As Object, e As EventArgs) Handles Timer4.Tick
+        If tem.Y > y4 Then
+            tem.Y -= 5
+        End If
+        If tem.Y < y4 Then
+            tem.Y += 5
+        End If
+        If tem.Y = y4 Then
+            Timer4.Stop()
+        End If
+        Panel1.Location = tem
+    End Sub
+
+    Public Sub falsifier()
+
     End Sub
 
 End Class
