3b1487552bc65bd162bfb8953e6930cec5d19fc8
Alexis Lyndon Galaura <alexislyndon@gmail.com>
2020-03-03 10:42:04 +0800

    good good

--- gateVIP.vb
+++ gateVIP.vb
@@ -4,23 +4,12 @@
     End Sub
 
     Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles TextBox1.TextChanged
-        If e.KeyChar = Microsoft.VisualBasic.ChrW(Keys.Return) Then
-            For l_index As Integer = 0 To ListBox1.Items.Count - 1
-                If CStr(ListBox1.Items(l_index)) = TextBox1.Text Then
-                    TextBox1.Clear()
-                    Return
-                End If
-            Next
 
-            ListBox1.Items.Add(TextBox1.Text.Trim)
-            TextBox1.Clear()
-            e.Handled = True
-        End If
     End Sub
 
     Private Sub TextBox1_KeyPress(sender As Object, e As KeyPressEventArgs) Handles TextBox1.KeyPress
         If e.KeyChar = Microsoft.VisualBasic.ChrW(Keys.Return) Then
-            Me.CompanyloginsTableAdapter1.CompanyIn(TextBox1.Text.Trim)
+            Me.CompanyloginsTableAdapter1.CompanyIn(TextBox1.Text.Trim, Session.s_gate)
         End If
     End Sub
 End Class
