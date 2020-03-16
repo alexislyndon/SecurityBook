8ae78ab4bd66d181dc9ab4bc41bc0a8e1bdab118
Alexis Lyndon Galaura <alexislyndon@gmail.com>
2020-03-16 21:26:42 +0800

    changed color of id capture

--- captureID.Designer.vb
+++ captureID.Designer.vb
@@ -166,6 +166,7 @@ Partial Class captureID
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.White
         Me.ClientSize = New System.Drawing.Size(516, 341)
         Me.ControlBox = False
         Me.Controls.Add(Me.closelbl)
